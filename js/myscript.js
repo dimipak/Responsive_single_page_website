6cca1c81287b0222186f6f6b6fa1db4e6d61f496
Dimitris <jimpak13@hotmail.com>
2017-05-13 01:00:54 +0300

    Enabled scrollspy with jquery for the navbar

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,18 @@
-﻿$('.carousel').carousel({		//$('#featured'). would work as well
-  interval: false
+﻿$(function() {
+	"use strict";
+	
+	var topoffset = 50; // variable for menu height
+	
+	//Activate Scrollspy
+	$('body').scrollspy({
+		target: 'header .navbar',
+		offset: topoffset
+	});
+	
+	
+	$('.carousel').carousel({		//$('#featured'). would work as well
+	  interval: false
+	});
+
+
 });
