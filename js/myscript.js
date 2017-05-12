b54f70f60b7e2f3d3eea4eae397cc0f6b7607dcc
Dimitris <jimpak13@hotmail.com>
2017-05-13 01:14:14 +0300

    Added javascript-jquery to track the scroll where it is so it can activate inbody class

--- js/myscript.js
+++ js/myscript.js
@@ -10,6 +10,34 @@
 	});
 	
 	
+	// finds and store the attribute of the anchor tag of the active class inside a li tag, in 'this' document
+	var hash = $(this).find('li.active a').attr('href');
+		
+	if(hash !== '#featured') {
+		$('header nav').addClass('inbody');
+	} else {
+		$('header nav').removeClass('inbody');
+	}
+	
+	
+	// Adds inbody class to nav when scrollspy event fires
+	$('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+		
+		// finds and store the attribute of the anchor tag of the active class inside a li tag, in 'this' document
+		var hash = $(this).find('li.active a').attr('href');
+		
+		if(hash !== '#featured') {
+			$('header nav').addClass('inbody');
+		} else {
+			$('header nav').removeClass('inbody');
+		}
+		
+	});
+	
+	
+	
+	
+	
 	$('.carousel').carousel({		//$('#featured'). would work as well
 	  interval: false
 	});
