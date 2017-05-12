5b9ba3fe1e46ffbde3c0751aba791831f48f5f27
Dimitris <jimpak13@hotmail.com>
2017-05-13 02:13:17 +0300

    Added smooth scrolling by jquery in myscript.js file

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 ﻿$(function() {
-	"use strict";
+	//"use strict";
 	
 	var topoffset = 50; // variable for menu height
 	
@@ -34,6 +34,20 @@
 		
 	});
 	
+	
+	//Use smooth scrolling when clicking on navigation
+	$('.navbar a').click(function() {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top-topoffset+2
+                }, 500);
+                return false;
+            } //target.length
+        } //click function
+    }); //smooth scrolling
 	
 	
 	
